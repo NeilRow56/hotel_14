ebef89d143e9b5efb1a1cf5a68be6ba2b6f900af
NeilRow56 <neilrowland56@gmail.com>
2024-02-03 11:39:56 +0000

    Added Clerk authentication

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { ClerkProvider } from '@clerk/nextjs'
+
 import './globals.css'
 import { ThemeProvider } from '@/components/ThemeProvider'
 import { Navbar } from '@/components/Navbar'
@@ -17,18 +19,20 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <Navbar />
-          {children}
-        </ThemeProvider>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en" suppressHydrationWarning={true}>
+        <body className={inter.className}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <Navbar />
+            {children}
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   )
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,14 @@
 import Link from 'next/link'
-
-import { Button } from './ui/button'
 import { ThemeToggle } from './ThemeToggle'
+import { Button } from './ui/button'
+import { UserButton } from '@clerk/nextjs'
 
 export function Navbar() {
   return (
     <nav className=" flex h-[8vh] items-center border-b bg-slate-200 dark:bg-slate-700">
       <div className="container mx-auto  flex items-center justify-between">
         <Link href="/">
-          <h1 className="text-3xl font-bold">Hotel 14</h1>
+          <h1 className="text-3xl font-bold">Sass Notes</h1>
         </Link>
         <div className="flex items-center gap-x-5">
           <ThemeToggle />
@@ -19,6 +19,7 @@ export function Navbar() {
             <Button variant="secondary" asChild>
               <Link href="/sign-up">Sign up</Link>
             </Button>
+            <UserButton afterSignOutUrl="/" />
           </div>
         </div>
       </div>
